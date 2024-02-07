c48926b31259831f0b506a7b5add7b86cdad17bb
Jhonna <jhonna.somarriba@gmail.com>
2024-02-06 23:11:56 -0600

    Circular progress and pageView

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,13 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // todo: Repository,DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // todo: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    // todo: cargar videos
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
